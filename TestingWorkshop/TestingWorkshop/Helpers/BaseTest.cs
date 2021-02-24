f46f2586d6c632fdef10b0ebe45d2c06be585800
Lucian Seican <lucian.seican@evozon.com>
2021-02-24 10:46:55 +0200

    Setup project structure

---
+++ TestingWorkshop/TestingWorkshop/Helpers/BaseTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NsTestFrameworkUI.Helpers;
+
+namespace TestingWorkshop.Helpers
+{
+    public class BaseTest : NsTestFrameworkUI.BaseTest
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Browser.InitializeDriver();
+            Browser.GoTo("https://automationintesting.online/#/");
+        }
+    }
+}
---
+++ TestingWorkshop/TestingWorkshop/Tests/BookingTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NsTestFrameworkUI.Helpers;
+using OfficeOpenXml.Packaging.Ionic.Zip;
+using TestingWorkshop.Helpers;
+
+namespace TestingWorkshop.Tests
+{
+    [TestClass]
+    public class BookingTests : BaseTest
+    {
+        [TestMethod]
+        public void WhenBookingRoomSuccessMessageShouldBeDisplayedTest()
+        {
+            WaitHelpers.ExplicitWait();
+        }
+    }
+}
