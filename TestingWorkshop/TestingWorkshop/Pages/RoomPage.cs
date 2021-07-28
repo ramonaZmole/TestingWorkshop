469d6dedab1ddb07f152631630718d0d2c17b844
emiliangrecu <emilian.grecu@evozon.com>
2021-07-28 10:38:46 +0300

    add selectors for new room

--- TestingWorkshop/TestingWorkshop/Pages/RoomPage.cs
+++ TestingWorkshop/TestingWorkshop/Pages/RoomPage.cs
@@ -15,6 +15,13 @@ namespace TestingWorkshop.Pages
         private readonly By _roomPriceInput = By.CssSelector("#roomPrice");
         private readonly By _roomDetailsCheckBoxes = By.CssSelector(".form-check-input");
         private readonly By _roomDetailsLabels = By.CssSelector(".form-check-label");
+
+        private readonly By _roomList = By.CssSelector(".row.detail");
+        private readonly By _roomNumber = By.CssSelector("[id*='roomNumber']");
+        private readonly By _type = By.CssSelector("[id*='type']");
+        private readonly By _accessible = By.CssSelector("[id*='accessible']");
+        private readonly By _roomPrice = By.CssSelector("[id*='roomPrice']");
+        private readonly By _details = By.CssSelector("[id*='details']");
         #endregion
 
 
