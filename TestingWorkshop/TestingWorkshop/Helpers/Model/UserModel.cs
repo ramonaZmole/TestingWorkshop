5eef5a7859ed9b0bc7e30a6f22c8cfe42645511b
Ramona Zmole <ramona.zmole@gmail.com>
2021-10-06 10:39:28 +0300

    fix tests

--- TestingWorkshop/TestingWorkshop/Helpers/Model/UserModel.cs
+++ TestingWorkshop/TestingWorkshop/Helpers/Model/UserModel.cs
@@ -2,8 +2,8 @@
 {
     public class UserModel
     {
-        public string FirstName { get; set; } = Faker.Name.FullName();
-        public string LastName { get; set; } = Faker.Name.FullName();
+        public string FirstName { get; set; } = Faker.Name.First();
+        public string LastName { get; set; } = Faker.Name.Last();
         public string Email { get; set; } = Faker.Internet.Email();
         public string ContactPhone { get; set; } = "89123749157";
     }
--- TestingWorkshop/TestingWorkshop/Pages/ReportPage.cs
+++ TestingWorkshop/TestingWorkshop/Pages/ReportPage.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using NsTestFrameworkUI.Helpers;
+﻿using NsTestFrameworkUI.Helpers;
+using NsTestFrameworkUI.Pages;
 using OpenQA.Selenium;
 
 namespace TestingWorkshop.Pages
@@ -8,6 +8,7 @@ namespace TestingWorkshop.Pages
     {
         public bool IsBookingDisplayed(string name, int roomId)
         {
+            PageHelpers.ScrollDownToView(1000);
             try
             {
                 Browser.WebDriver.FindElement(By.XPath($"//*[contains(text(), '{name} - Room: {roomId}')]"));
